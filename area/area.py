4fe95f05fb50ea4a6244119a4e2ec610ed7810d6
«Selezneva <petrukhina.marina2011@yandex.ru>
2024-03-14 15:15:29 +0300

    lint

--- area/area.py
+++ area/area.py
@@ -1,24 +1,24 @@
 from math import pi, pow, sqrt
 
+
 def get_area(a, b=None, c=None):
-    #круг#
-    if type_figure(a, b, c) == "circle": 
+    # Круг#
+    if type_figure(a, b, c) == "circle":
         return pi * pow(a, 2)
-     #треугольник#
+    # Треугольник#
     elif type_figure(a, b, c) == "trangle" or type_figure(a, b, c) == "right trangle":
         p = (a + b + c) / 2
         return sqrt(p * (p - a) * (p - b) * (p - c))
-    
+
 
 def type_figure(a, b=None, c=None):
 
     if b is None and c is None and a > 0:
         return "circle"
-    
     elif b is not None and c is not None and a + b > c and b + c > a and c + a > b:
         if pow(a, 2) + pow(b, 2) == pow(c, 2) or pow(c, 2) + pow(b, 2) == pow(a, 2) or pow(a, 2) + pow(c, 2) == pow(b, 2):
             return "right trangle"
         return "trangle"
-    #сюда можно добавить другие фигуры
+    # Сюда можно добавить другие фигуры
     else:
         raise ValueError('Некорректные параметры фигуры')
--- area/scripts/area.py
+++ area/scripts/area.py
@@ -1,5 +1,12 @@
+from area import get_area
+# a =
+# b =
+# c =
+
+
 def main():
-    return get_area(a,b=None,c=None)
+    return get_area(a, b=None, c=None)
+
 
 if __name__ == '__main__':
     main()
